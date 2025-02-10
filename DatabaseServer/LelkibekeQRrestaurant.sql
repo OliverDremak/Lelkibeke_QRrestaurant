c76a53b2434579331975097c0350d5b8533f03d5
kissmarcell132 <kiss.marcell@students.jedlik.eu>
2025-02-10 17:18:26 +0100

    database in work

--- DatabaseServer/LelkibekeQRrestaurant.sql
+++ DatabaseServer/LelkibekeQRrestaurant.sql
@@ -3,12 +3,13 @@ CREATE DATABASE LelkibekeQR
 	COLLATE utf8_hungarian_ci;
 
 CREATE TABLE IF NOT EXISTS `users` (
-	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
-	`email` varchar(255) NOT NULL,
-	`password` varchar(255) NOT NULL,
-	`name` varchar(255) NOT NULL,
-	`role` enum NOT NULL,
-	PRIMARY KEY (`id`)
+    `id` INT AUTO_INCREMENT NOT NULL UNIQUE,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `role` ENUM('admin', 'user', 'waiter') NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE (`email`)
 );
 
 CREATE TABLE IF NOT EXISTS `tables` (
@@ -21,7 +22,7 @@ CREATE TABLE IF NOT EXISTS `tables` (
 
 CREATE TABLE IF NOT EXISTS `menu_items` (
 	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
-	`category_id` varchar(255) NOT NULL,
+	`category_id` int NOT NULL,
 	`name` varchar(255) NOT NULL,
 	`description` text NOT NULL,
 	`price` decimal(10,0) NOT NULL,
@@ -33,10 +34,10 @@ CREATE TABLE IF NOT EXISTS `orders` (
 	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
 	`user_id` int NOT NULL,
 	`table_id` int NOT NULL,
-	`status` text NOT NULL,
+	`status` enum('cooking', 'done') NOT NULL,
 	`total_price` text NOT NULL,
 	`created_at` timestamp NOT NULL,
-	`updated_at` timestamp NOT NULL,
+-- 	`updated_at` timestamp NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
@@ -64,11 +65,45 @@ ALTER TABLE `orders` ADD CONSTRAINT `orders_fk2` FOREIGN KEY (`table_id`) REFERE
 ALTER TABLE `order_items` ADD CONSTRAINT `order_items_fk1` FOREIGN KEY (`order_id`) REFERENCES `orders`(`id`);
 
 ALTER TABLE `order_items` ADD CONSTRAINT `order_items_fk2` FOREIGN KEY (`menu_item_id`) REFERENCES `menu_items`(`id`);
-ALTER TABLE `category` ADD CONSTRAINT `category_fk0` FOREIGN KEY (`id`) REFERENCES `menu_items`(`category_id`);
+ALTER TABLE `menu_items` ADD CONSTRAINT `menu_items_fk1` FOREIGN KEY (`category_id`) REFERENCES `category`(`id`);
+
+
 
 DELIMITER //
 CREATE PROCEDURE GetUsers()
 BEGIN
     SELECT * FROM users;
 END //
-DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE GetMenu()
+BEGIN
+  SELECT * FROM `menu_items`;
+END //
+
+
+DELIMITER //
+CREATE PROCEDURE GetActiveOrders()
+BEGIN
+    SELECT 
+        orders.table_id, 
+        GROUP_CONCAT(menu_items.name SEPARATOR ', ') AS menu_items
+    FROM orders
+    JOIN order_items ON orders.id = order_items.order_id
+    JOIN menu_items ON order_items.menu_item_id = menu_items.id
+    WHERE orders.status = 'cooking'
+    ORDER BY orders.table_id, menu_items.name;
+END //
+
+DELIMITER //
+CREATE PROCEDURE GetAllOrderedItems()
+BEGIN
+    SELECT GROUP_CONCAT(menu_items.name SEPARATOR ', ')
+    FROM orders
+    JOIN order_items ON orders.id = order_items.order_id
+    JOIN menu_items ON order_items.menu_item_id = menu_items.id
+    WHERE orders.status = 'cooking'
+    ORDER BY menu_items.name;
+END //
+
+
