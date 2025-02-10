f5c61690ed9f4afb445f7cc8a13d6458e02fbcad
OliverDremak <dremakoliver@gmail.com>
2025-02-10 16:24:36 +0100

    page added

---
+++ FrontEnd/LelkiBekeFrontEnd/pages/menu.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+
+const goToLogin = () => {
+  router.push('/login');
+};
+</script>
+
+<template>
+  <div>
+    <Navbar />
+    <RestaurantMenu />
+    <Footer />
+  </div>
+</template>
