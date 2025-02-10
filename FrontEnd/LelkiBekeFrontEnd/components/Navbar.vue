06cb09807fd15ea397b2b12b2074f4c9bb205b00
OliverDremak <dremakoliver@gmail.com>
2025-02-10 16:19:05 +0100

    Component added

---
+++ FrontEnd/LelkiBekeFrontEnd/components/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Lelkibéke</a>
+      <button class="navbar-toggler" @click="toggleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div :class="{'collapse navbar-collapse': !isNavbarOpen, 'navbar-collapse': isNavbarOpen}" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item m-2">
+            <ButtonComponet @click="goToRegister" text="Register" class="w-100"/>
+          </li>            
+          <li class="nav-item m-2">             
+            <ButtonComponet @click="goToLogin" text="Login" class="w-100"/>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+  
+  <script setup lang="ts">
+  import { useRouter } from 'vue-router'
+  import { ref } from 'vue';
+import ButtonComponet from './ButtonComponet.vue';
+
+const isNavbarOpen = ref(false);
+const toggleNavbar = () => {
+  isNavbarOpen.value = !isNavbarOpen.value;
+};
+
+  
+  const router = useRouter()
+  
+  const goToLogin = () => {
+    router.push('/login')
+  }
+  const goToRegister = () => {
+    router.push('/register')
+  }
+  </script>
+  
+  <style scoped>
+  .navbar {
+    margin-bottom: 20px;
+  }
+  </style>
