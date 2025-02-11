39034d524283f2317a945bced8d647fe68ef1ad1
OliverDremak <dremakoliver@gmail.com>
2025-02-11 21:24:12 +0100

    send Order Created - user auth needed

--- Lelkibeke/routes/web.php
+++ Lelkibeke/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\MenuItemController;
+use App\Http\Controllers\OrderController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +11,4 @@ Route::get('/', function () {
 
 Route::get('/users', [UserController::class, 'getUsers']);
 Route::get('/menu', [MenuItemController::class, 'getMenu']);
+//Route::post('/sendorder', [OrderController::class, 'sendOrder']); //Meg nem jo kell a useres rész
