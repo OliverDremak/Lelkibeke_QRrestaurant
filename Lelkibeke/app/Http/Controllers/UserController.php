25fd1b49bbc7f374e3d922e65bd27211f08362ea
Oliver Dremak <dremakoliver@gmail.com>
2025-02-11 09:15:19 +0100

    Backend updated

--- Lelkibeke/app/Http/Controllers/UserController.php
+++ Lelkibeke/app/Http/Controllers/UserController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    //
+    public function getUsers()
+    {
+        // Tárolt eljárás meghívása
+        $users = DB::select('CALL GetUsers()');
+
+        // Visszaküldjük a lekérdezett adatokat JSON formátumban
+        return response()->json($users);
+    }
 }
